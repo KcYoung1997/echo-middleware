77fca45e1db164237720d63d4b6bbd2eebef42ae
Keiran Young <KcYoung1997@hotmail.co.uk>
2020-11-28 19:57:46 +0000

    Return http 200

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = (req, res, next) => {
+    res.status(200);
     res.write(`${req.method} ${req.originalUrl} HTTP/${req.httpVersion}`);
     Object.entries(req.headers).forEach(([k,v]) => { 
         res.write(`\n${k}: ${v}`);
