2895e3e0914828aded91bf5098e5b9b2639d3125
Keiran Young <KcYoung1997@hotmail.co.uk>
2020-11-28 20:54:50 +0000

    Added server compatibility tests

--- index.test.js
+++ index.test.js
@@ -1,38 +1,77 @@
-const app = require('express')();
 const echo = require('.');
-const request = require('supertest')(app);
-var server;
+const request = require('supertest');
 
-beforeAll(async () => {
-    app.use(echo);
-    server = await app.listen(1337);
+
+describe('test server compatibility', () => {
+    it('express', async () => {
+        const app = require('express')();
+        app.use(echo);
+        const server = await app.listen(8080);
+        await request(app).get('/').expect(/^GET/);
+        await server.close();
+    });
+
+    it('connect', async () => {
+        const app =  require('connect')();
+        app.use(echo);
+        const server = await require('http').createServer(app).listen(8080);
+        await request(app).get('/').expect(/^GET/);
+        await server.close();
+    });
+
+    it('koa', async () => {
+        const Koa = require('koa');
+        const app = new Koa();
+        const http = require('http');
+        
+        app.use((ctx, next) => {
+            if(ctx.path === '/echo') {
+                ctx.status = 200;
+                echo(ctx.req, ctx.res, next); 
+            }
+            else next();
+        });
+        const server = await app.listen(8080);
+        await request('localhost:8080').get('/echo').expect(/^GET/);
+        await server.close();
+    });
 });
 
-afterAll(() => {
-    server.close();
-});
+describe('check return contents', () => {
+    const app = require('express')();
+    var server;
 
-it('returns get', done => {
-    request
-        .get('/')
-        .expect(/^GET/, done);
-});
+    beforeAll(async () => {
+        app.use(echo);
+        server = await app.listen(8080);
+    });
+    
+    afterAll(() => {
+        server.close();
+    });
 
-it('returns post', done => {
-    request
-        .post('/')
-        .expect(/^POST/, done);
-});
+    it('returns get', done => {
+        request(app)
+            .get('/')
+            .expect(/^GET/, done);
+    });
 
-it('returns path', done => {
-    request
-        .get('/test')
-        .expect(/^GET \/test/, done);
-});
+    it('returns post', done => {
+        request(app)
+            .post('/')
+            .expect(/^POST/, done);
+    });
 
-it('returns header', done => {
-    request
-        .get('/')
-        .set('returns_header', 'test')
-        .expect(/\nreturns_header: test\n/, done);
+    it('returns path', done => {
+        request(app)
+            .get('/test')
+            .expect(/^GET \/test/, done);
+    });
+
+    it('returns header', done => {
+        request(app)
+            .get('/')
+            .set('returns_header', 'test')
+            .expect(/\nreturns_header: test\n/, done);
+    });
 });
